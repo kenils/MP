141858140ba8d899e72eb1101659ecb9e529bfac
kenils <57350900+kenils@users.noreply.github.com>
2019-11-06 02:53:22 +0530

    Add files via upload

---
+++ MAX.asm
@@ -0,0 +1,98 @@
+Data Segment
+msg1 db 10,13,"Enter number of elemenets$"
+msg2 db 10,13,"Enter the numbers:$"
+msg3 db 10,13,"$"
+msg4 db 10,13,"The max of the given numbers is:$"
+
+n db ?
+max db ?
+Data Ends
+Code Segment
+Assume CS:Code,DS:Data
+Start:
+mov ax,data
+mov ds,ax
+
+mov dx,offset msg1
+mov ah,09h
+int 21h
+
+call takenumber
+mov n,al
+
+mov dx,offset msg2
+mov ah,09h
+int 21h
+
+mov si,1000h
+mov cl,n
+mov ch,00h
+mov max,00h  ;initializing max=0
+
+A:
+call takenumber ;will take 8 bit number and store in al
+mov [si],al
+
+cmp al,max     ; if element < max then continue else max=element
+jc B
+mov max,al 
+
+B:
+mov dx,offset msg3
+mov ah,09h          ;takes the cursor to the next line
+int 21h
+
+inc si
+loop A
+
+mov dx,offset msg4
+mov ah,09h
+int 21h
+
+mov bl,max
+and bl,0f0h
+ror bl,04h
+call output
+
+mov bl,max
+and bl,0fh
+call output
+
+mov ax,4c00h
+int 21h
+
+input proc
+mov ah,01h
+int 21h
+cmp al,41h
+jc laba
+sub al,07h
+laba:
+sub al,30h
+ret
+endp
+
+output proc
+cmp bl,0Ah
+jc labd
+add bl,07h
+labd:
+add bl,30h
+mov ah,02h
+mov dl,bl
+int 21h
+ret
+endp
+
+takenumber proc
+call input
+rol al,04h
+mov bl,al
+
+call input
+add al,bl
+ret
+endp
+
+Code Ends
+ End Start
